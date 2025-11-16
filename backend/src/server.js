c7a40d4b336930acc2a34217f041dceac688e594
nadyazhigarevich <zhigarevichnadya@gmail.com>
2025-11-16 10:16:31 +0300

    Add: rateLimiter; feat: ensure database connection before server startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,25 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv" ;
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-connectDB();
-
+//middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log('Server is running on port:', PORT);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port:', PORT);
+    });
 });
+
+
 
