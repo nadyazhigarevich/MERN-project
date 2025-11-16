4e4d42bc4cfa4f314c3d70029d9a836c47985ee6
nadyazhigarevich <zhigarevichnadya@gmail.com>
2025-11-16 09:40:08 +0300

    Add: controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv" 
+import dotenv from "dotenv" ;
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 connectDB();
+
+app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 
